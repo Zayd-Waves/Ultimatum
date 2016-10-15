e7a30e87f720579bcc0787b097582685734f3516
Zayd Bille <zayd.tri@gmail.com>
2016-10-15 03:18:55 -0400

    start working dashboard

--- Ultimatum/app/src/main/java/com/electricpanda/ultimatum/SplashActivity.java
+++ Ultimatum/app/src/main/java/com/electricpanda/ultimatum/SplashActivity.java
@@ -1,12 +1,40 @@
 package com.electricpanda.ultimatum;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        boolean isItTheFirstLaunch = PreferencesManager.shouldShowAppIntro(mContext);
+        if(isItTheFirstLaunch) {
+
+            startAppIntroduction();
+
+            /* After this launch, we don't need to show the intro anymore. So we'll hide it. */
+            PreferencesManager.hideAppIntro(mContext);
+
+        } else {
+            startApp();
+        }
+    }
+
+    private void startAppIntroduction() {
+        Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
